04d500fe2ee38697e733014657d5be81c36f821c
YoussraAkdimi <y_akdimi@hotmail.com>
2021-06-22 16:02:58 +0200

    pshader

--- INFOGR2021tmpl8_P2/shaders/fs.glsl
+++ INFOGR2021tmpl8_P2/shaders/fs.glsl
@@ -10,9 +10,9 @@ uniform sampler2D pixels;	// texture sampler
 // shader output
 out vec4 outputColor;
 
-uniform vec3 lightPos = vec3(50, 50, -100);
+uniform vec3 lightPos = vec3(-350, -150, 600);
 uniform vec3 lightColor = vec3(0.5, 0.5, 0.5);
-uniform vec3 viewPos = vec3(0, 0, -15);
+uniform vec3 viewPos = vec3(0, 0, -50);
 uniform vec3 objectColor;
 
 // fragment shader
@@ -20,25 +20,25 @@ void main()
 {
     //ambient
     float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
+    //vec3 ambient = ambientStrength * lightColor;
 
     //diffuse
-    vec4 lightDir = vec4(lightPos, 1) - vec4(fragPos, 1);
+    vec4 lightDir = vec4(fragPos, 1) - vec4(lightPos, 1);
     vec4 norm = normalize(normal);
     vec4 lightDirN = normalize(lightDir);
     float diff = dot(norm, lightDirN);
     diff = max(diff, 0.0);
-    vec3 diffuse = diff * lightColor;
+    //vec3 diffuse = diff * lightColor;
 
     //specular
-    float specularStrength = 0.000000001;
+    float specularStrength = 0.01;
     vec4 viewDir = normalize(vec4(viewPos,1) - vec4(fragPos,1));
     vec4 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 8);
-    vec3 specular = specularStrength * spec * lightColor;
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 1);
+    //vec3 specular = specularStrength * spec * lightColor;
 
 
     //combining shaders
-    vec3 result = (ambient + diffuse + specular);
+    vec3 result = (ambientStrength + diff + (spec*specularStrength)) * lightColor;
     outputColor = texture( pixels, uv ) + vec4(result, 1);
 }
